73cfdae01b9f26a39c4a8908eb3773d3bcba3bab
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2025-02-24 15:26:12 -0600

    styling change

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${font.className} dark min-h-dvh overflow-x-hidden antialiased`}
+        className={`${font.className} dark min-h-dvh overflow-x-hidden antialiased bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] [--pattern-fg:var(--color-muted)]/50 dark:[--pattern-fg:var(--color-muted)]/10`}
       >
         {children}
         <Toaster />
