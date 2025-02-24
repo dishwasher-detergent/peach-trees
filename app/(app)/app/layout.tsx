177f817b14a6e3753bd09a48b5070252cdded30c
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2025-02-24 11:36:07 -0600

    Update layout.tsx

--- app/(app)/app/layout.tsx
+++ app/(app)/app/layout.tsx
@@ -10,7 +10,7 @@ export default function AppLayout({
   return (
     <>
       <Nav />
-      <main className="mx-auto h-full max-w-6xl p-4 px-4 md:px-8">
+      <main className="mx-auto h-full max-w-6xl p-4 px-4 md:px-8 bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] [--pattern-fg:var(--color-muted)]/50 dark:[--pattern-fg:var(--color-muted)]/10">
         <section className="flex flex-row items-center justify-between pb-4">
           <h1 className="text-lg font-bold">Your Habits</h1>
           <AddGoal />
